080e20f3b1f6e2029e08821692bc7696a03576f7
SAGEROMEO <104103422+SAGEROMEO@users.noreply.github.com>
2026-03-26 19:34:45 +0200

    Subclass of animal

---
+++ Elephant.java
@@ -0,0 +1,89 @@
+public class Elephant extends Animal
+{
+    private String name;
+
+    private String species;
+
+    private String sound = "trumpet";
+
+    private String limbs = "four legs";
+
+    private String habitat = "savannah and forests";
+
+    Elephant(){}
+
+    Elephant(String name, String species)
+    {
+        this.name = name;
+        this.species = species;
+    }
+
+    Elephant(String name, String species,String sound,String limbs, String habitat)
+    {
+        this.name = name;
+        this.species = species;
+        this.sound =sound;
+        this.limbs = limbs;
+        this.habitat = habitat;
+
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getSpecies()
+    {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getSound()
+    {
+        return sound;
+    }
+
+    public void setSound(String sound)
+    {
+        this.sound = sound;
+    }
+
+    public String getLimbs()
+    {
+        return limbs;
+    }
+
+    public void setLimbs(String limbs)
+    {
+        this.limbs = limbs;
+    }
+
+    public void setHabitat(String habitat)
+    {
+        this.habitat = habitat;
+    }
+
+    public String getHabitat()
+    {
+        return habitat;
+    }
+
+    @Override
+    void display()
+    {
+
+        System.out.println("Animal name: " + getName() + "\nAnimal species: " + getSpecies() + "\nAnimal sound: "
+                + getSound() + "\nAnimal Limbs: " + getLimbs() + "\nAnimal habitat: " + getHabitat() + "\n");
+
+
+    }
+}
